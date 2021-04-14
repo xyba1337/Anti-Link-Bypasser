6c702f43af3944adb27d7dd9c2dcfe5154be81f2
xyba1337 <62798507+xyba1337@users.noreply.github.com>
2021-04-14 12:02:52 +0200

    Update antibypass.php

--- antibypass.php
+++ antibypass.php
@@ -23,3 +23,4 @@ function checkforbypass($check_for_header)
 }
 
 checkforbypass(strpos($_SERVER["HTTP_REFERER"], "https://example.com/"));//e.g. add https://linkvertise.com/ - in order to prevent people from skipping the linkvertise link
+?>
